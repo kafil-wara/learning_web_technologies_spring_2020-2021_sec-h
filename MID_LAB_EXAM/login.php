1595a2c388ab19959a44c495bd8cd60bbc3b0fea
kafil-wara <kafilwr@gmail.com>
2021-03-14 13:14:14 +0600

    labexam

---
+++ MID_LAB_EXAM/login.php
@@ -0,0 +1,36 @@
+<?php
+    session_start();
+     
+   
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Page</title>
+</head>
+<body>
+
+	<center>
+	<form>
+		<table border="0" cellspacing="0" cellpadding="0">
+			<tr>
+				<td>
+					<fieldset>
+						<legend><h3>LOGIN</h3></legend>
+						User Id<br/>
+						<input type="text"><br/>                               
+						Password<br/>
+						<input type="password">
+						<br /><hr/>
+						<input type="button" value="Login">
+						<a href="registration.html">Register</a>
+					</fieldset>
+				</td>
+			</tr>                                
+		</table>
+	</form>
+	</center>
+
+</body>
+</html>
